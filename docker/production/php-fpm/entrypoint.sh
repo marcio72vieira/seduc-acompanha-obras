2966075d1d0a8b22fb281bce827d54b55ec5f9dd
marcio72vieira <marcio72vieira@hotmail.com>
2025-05-20 17:34:45 -0300

    Substituindo pasta docker e arquuivos dockercomposer

--- docker/production/php-fpm/entrypoint.sh
+++ docker/production/php-fpm/entrypoint.sh
@@ -3,8 +3,8 @@ set -e
 
 main() {
     prepare_storage
-    optimize_app
     run_migrations
+    optimize_app
     exec "$@"
 }
 
@@ -14,8 +14,14 @@ prepare_storage() {
     mkdir -p /var/www/storage/framework/views
 
     chown -R www-data:www-data /var/www/storage
+    chown -R www-data:www-data /var/www/vendor/mpdf
 
-    php artisan storage:link
+    # Verifica se o link simbólico já existe antes de criar
+    if [ ! -L /var/www/public/storage ]; then
+        php artisan storage:link
+    else
+        echo "Link simbólico public/storage já existe."
+    fi
 }
 
 wait_for_db() {
